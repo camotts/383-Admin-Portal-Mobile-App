c2c631e0dd01284808b9fb290a8083131a3deadb
RAHUL SHRESTHA <rahul.shrestha@selu.edu>
2015-04-25 00:26:34 -0500

    changes to Dustin's upload. just some moving of files.

--- GameStoreMobileApp/Droid/obj/Debug/android/gamestoremobileapp/droid/R.java
+++ GameStoreMobileApp/Droid/obj/Debug/android/gamestoremobileapp/droid/R.java
@@ -14,5 +14,9 @@ public final class R {
         public static final int 21=0x7f020000;
         public static final int home_background=0x7f020001;
         public static final int icon=0x7f020002;
+        public static final int splash=0x7f020003;
+    }
+    public static final class style {
+        public static final int Theme_Splash=0x7f030000;
     }
 }
