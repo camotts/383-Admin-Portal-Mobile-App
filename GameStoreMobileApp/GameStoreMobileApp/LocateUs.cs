f77ac6345b72f98db12951b78a6a3b1c4c0c885e
RAHUL SHRESTHA <rahul.shrestha@selu.edu>
2015-04-26 22:24:19 -0500

    added google maps api for cross platform. user can search for our store, and get direction to the store from his location. two stores, A and B have been pinned. Tested for iOS. work in progress for android.

--- GameStoreMobileApp/GameStoreMobileApp/LocateUs.cs
+++ GameStoreMobileApp/GameStoreMobileApp/LocateUs.cs
@@ -4,34 +4,15 @@ using Xamarin.Forms.Maps;
 
 namespace GameStoreMobileApp
 {
-	public class LocateUs : ContentPage
+	public class LocateUs : TabbedPage
 	{
 		public LocateUs ()
 		{
-			var map = new Map(
-				MapSpan.FromCenterAndRadius(
-					new Position(37,-122), Distance.FromMiles(0.3))) {
-				IsShowingUser = true,
-				HeightRequest = 100,
-				WidthRequest = 960,
-				VerticalOptions = LayoutOptions.FillAndExpand
-			};
+			this.Title = "Locate Us";
 
-			// add the slider
-			var slider = new Slider (1, 18, 1);
-			slider.ValueChanged += (sender, e) => {
-				var zoomLevel = e.NewValue; // between 1 and 18
-				var latlongdegrees = 360 / (Math.Pow(2, zoomLevel));
-				//Debug.WriteLine(zoomLevel + " -> " + latlongdegrees);
-				if (map.VisibleRegion != null)
-					map.MoveToRegion(new MapSpan (map.VisibleRegion.Center, latlongdegrees, latlongdegrees)); 
-			};
-
-			var stackLayout = new StackLayout { Spacing = 0 };
-			stackLayout.Children.Add(map);
-			stackLayout.Children.Add (slider);
-
-			Content = stackLayout;
+			Children.Add (new StoreMapPage {Title="Our Stores",Icon="glypish_globe.png"});
+			Children.Add (new GetTherePage {Title="Get There",Icon="glypish_way.png"});
+		
 		}
 	}
 }
