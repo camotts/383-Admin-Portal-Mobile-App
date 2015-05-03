990ec074de486d798a42f60c1089f011fae4d142
RAHUL SHRESTHA <rahul.shrestha@selu.edu>
2015-05-02 23:57:54 -0500

    using application context to store user sessions. applied BTProgressHUD to iOS (need to work on dependency service for it to work on android). everything done, just need to add the game info pages and checkout stuffs (working on it).

--- GameStoreMobileApp/GameStoreMobileApp/Repositories/UserRepository.cs
+++ GameStoreMobileApp/GameStoreMobileApp/Repositories/UserRepository.cs
@@ -3,6 +3,7 @@ using Xamarin.Forms;
 using RestSharp;
 using RestSharp.Deserializers;
 using System.Net;
+using System.Threading.Tasks;
 
 namespace GameStoreMobileApp
 {
@@ -13,23 +14,26 @@ namespace GameStoreMobileApp
 		{
 
 		}
-
-	
-		public Boolean OnLogInButtonClicked (string email, string password)
+			
+		public  Boolean OnLogInButtonClicked (string email, string password)
 		{
+			var hud = DependencyService.Get<IHud> ();
+			hud.Show ("Verifying Credentials");
 			var client = new RestClient("http://dev.envocsupport.com/GameStore2/");
 			var request = new RestRequest("api/ApiKey?email=" + email + "&password=" + password, Method.GET);
 
 			var queryResult = client.Execute(request);
 
+
+
 			if (queryResult.StatusCode == HttpStatusCode.OK)
 			{
-//				var deserial = new JsonDeserializer();
-//				var x = deserial.Deserialize<ApiKey>(queryResult);
-//
-//				var storeCookies = new CookieContainer();
-//				client.CookieContainer = storeCookies;
-			
+				var deserial = new JsonDeserializer();
+				var x = deserial.Deserialize<ApiKey>(queryResult);
+							
+				Application.Current.Properties["ApiKey"] = x.UserApiKey;
+				Application.Current.Properties ["UserId"] = x.UserId;
+
 				loginStatus = true;
 				return loginStatus ;
 			}
@@ -37,10 +41,12 @@ namespace GameStoreMobileApp
 			{
 				loginStatus = false;
 				return loginStatus;
-				//DisplayAlert ("Invalid Credentials", "Please Check your Email or Password!", "OK");
+
 			}
 
 		}
+
+	
 	}
 }
 
---
+++ GameStoreMobileApp/GameStoreMobileApp/Views/LookAround.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace GameStoreMobileApp
+{
+	public class LookAround: ContentPage
+	{
+		public LookAround ()
+		{
+			var hud = DependencyService.Get<IHud> ();
+
+			this.Title = "Store";
+
+			hud.Show("Loading Store");
+
+			var label = new Label { 
+				Text = "This is after user Logs IN."  
+			};
+
+			var layout = new StackLayout ();
+
+			layout.Children.Add (new BoxView {Color = Color.Transparent, HeightRequest = 50});
+			layout.Children.Add (label);
+
+			Content = layout;
+		}
+
+
+	}
+}
+
